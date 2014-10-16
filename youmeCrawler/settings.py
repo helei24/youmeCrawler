831a833749a46e3bc72a4ab0c92f1cfbaf1000ac
He Lei <he.lei@integrasco.no>
2014-10-16 22:57:10 +0800

    first commit

---
+++ youmeCrawler/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for youmeCrawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'youmeCrawler'
+
+SPIDER_MODULES = ['youmeCrawler.spiders']
+NEWSPIDER_MODULE = 'youmeCrawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'youmeCrawler (+http://www.yourdomain.com)'
